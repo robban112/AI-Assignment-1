9d53362a41beb1b13d179a15a22849bf51439a3f
Robert <robbanl@kth.se>
2017-09-12 16:32:47 +0200

    reestimate B

--- AI-A1-IntelliJ-project/src/BaumWelch.java
+++ AI-A1-IntelliJ-project/src/BaumWelch.java
@@ -92,4 +92,20 @@ public class BaumWelch {
   private void run() {
 
   }
+
+  private void reestimateB() {
+    for(int i = 0; i<n; i++) {
+      for(int j = 0; j<m; j++) {
+        double numer = 0;
+        double denom = 0;
+        for(int t = 0; t<maxT; t++){
+          denom += gamma[t][i];
+          if (obsSeq[t] == j){
+            numer += gamma[t][i];
+          }
+        }
+      }
+      model.b[i][j] = numer / denom;
+    }
+  }
 }
